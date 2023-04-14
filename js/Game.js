cbb00f26a6f9793401d111241b39684d23e9deee
Admiin <silntmind@gmail.com>
2023-04-13 21:52:29 -0700

    moved the phrase methods to the phrase class
    moved the phrase methods to the phrase class

--- js/Game.js
+++ js/Game.js
@@ -19,9 +19,9 @@ class Game {
     startgame(){
         console.log('start game');
         document.querySelector('#overlay').style.display = "none";
-        this.activePhrase = this.getRandomPhrase(this.phrases);
-        console.log(this.activePhrase);
-        this.addPhraseToDisplay(this.activePhrase);
+        this.activePhrase = new Phrase(this.getRandomPhrase(this.phrases));
+        console.log(this.activePhrase.phrase);
+        this.activePhrase.addPhraseToDisplay(this.activePhrase);
         //sets this.activePhrase = this.getRandomPhrase();
         //once this.activePhrase set, run this.addPhraseToDisplay(activePhrase)
     }
@@ -32,64 +32,22 @@ class Game {
         return this.phrases[rollDice];
     }
 
-    addPhraseToDisplay(activePhrase){
-
-        //loads the hidden tiles for the letters in this.activePhrase
-        //we're going to need to loop through all the characters and create an LI element for each, assigning a letter or space class
-        let phraseUL = document.querySelector('#phrase').firstElementChild; //https://developer.mozilla.org/en-US/docs/Web/API/Element/firstElementChild
-        let phrase = this.activePhrase;
-        for(let i = 0; i < this.activePhrase.length; i++){
-            let newLI = document.createElement('LI');
-            if (phrase[i] === ' '){
-                newLI.textContent = phrase[i];
-                newLI.classList.add('space');
-                phraseUL.appendChild(newLI);
-            }else{
-                newLI.textContent = phrase[i];
-                newLI.classList.add('letter');
-                phraseUL.appendChild(newLI);
-            }
-        }
-    }
-
     handleInteraction(e){ //handles most of the game logic
         let key = e.innerHTML;
-            //addEventListener => click --- trigger: a letter is clicked
-                //disable clicked letter
-                //if then -> right or wrong letter clicked?
-                    //if right ->
-                        //add "chosen" class to letter
-                        //player.showMatchedLetter()
-                        //this.checkWin() -> if won -> this.gameOver()
-                    //if wrong ->
-                        //add "wrong" class to letter element
-                        //this.removeLife();
-        let letterList = document.querySelector('#phrase').firstElementChild.children;
+
         let livesCheck = false; //zeroLives by another name
-        let correctLetter = false;
-        for(let i = 0; i < letterList.length; i++){
-            if(letterList[i].innerHTML.toUpperCase() === key.toUpperCase()){
-                for(let i = 0; i < letterList.length; i++) {
-                    if(letterList[i].innerHTML.toUpperCase() === key.toUpperCase()){
-                        letterList[i].classList.add('show');
-                        letterList[i].classList.remove('letter');
-                    }
+        let letterCheck = this.activePhrase.checkLetter(key);
 
-                    e.classList.add('chosen');
-                }
-                correctLetter = true;
-                break;
-            }
-        }
-
-        if(!correctLetter){
+        if(!letterCheck){
             livesCheck = this.removeLife();
             e.classList.add('wrong');
-            e.disabled = true; //https://www.w3schools.com/jsref/prop_pushbutton_disabled.asp
+
+        }else{
+            e.classList.add('chosen');
         }
 
-
-
+        e.disabled = true; //https://www.w3schools.com/jsref/prop_pushbutton_disabled.asp
+        let letterList = document.querySelector('#phrase').firstElementChild.children;
         this.checkForWin(livesCheck, letterList);
     }
 
--- js/Phrase.js
+++ js/Phrase.js
@@ -11,16 +11,51 @@ class Phrase {     // what we are guessing
         //for each letter in phrase create a "hidden" letter block on screen. Make sure to have spaces where the
         //phrase has spaces, but don't load "blocked letter" class for spaces.
         //Make each letter an element rendered to the page? This would allow you to assign classes
+        //loads the hidden tiles for the letters in this.activePhrase
+        //we're going to need to loop through all the characters and create an LI element for each, assigning a letter or space class
+        let phraseUL = document.querySelector('#phrase').firstElementChild; //https://developer.mozilla.org/en-US/docs/Web/API/Element/firstElementChild
+        for(let i = 0; i < this.phrase.length; i++){
+            let newLI = document.createElement('LI');
+            if (this.phrase[i] === ' '){
+                newLI.textContent = this.phrase[i];
+                newLI.classList.add('space');
+                phraseUL.appendChild(newLI);
+            }else{
+                newLI.textContent = this.phrase[i];
+                newLI.classList.add('letter');
+                phraseUL.appendChild(newLI);
+            }
+        }
     }
 
     checkLetter(letter){
         let _letter = letter;
+        let result = false;
+        let letterList = document.querySelector('#phrase').firstElementChild.children;
         //cycle through the phrase elements we loaded earlier with addPhraseToDisplay.
         //if any letters match => showMatchedLetter method
         //if no letters match => player lose life method
+        for(let i = 0; i < letterList.length; i++){
+            if(letterList[i].innerHTML.toUpperCase() === _letter.toUpperCase()){
+                this.showMatchedLetter(letterList, _letter);
+                result = true;
+                break;
+            }
+        }
+        return result;
     }
 
-    showMatchedLetter(){
+    showMatchedLetter(letterList, e){
+        let _letterList = letterList;
+        let _e = e;
         //cycle through all the phrase elements and change their style from hidden to unhidden (probably a css class)
+        for(let i = 0; i < _letterList.length; i++) {
+            if(_letterList[i].innerHTML.toUpperCase() === _e.toUpperCase()){
+                _letterList[i].classList.add('show');
+                _letterList[i].classList.remove('letter');
+            }
+
+
+        }
     }
 }
